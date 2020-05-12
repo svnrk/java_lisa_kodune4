4e2051659ff9cdc2bf27b68c4365bb792fc4180a
svnrk <svenerikmandmaa@gmail.com>
2020-05-12 22:53:44 +0300

    Klasside tesimine lisatud nüüd ka main.java klassi. Eelnevalt olid selle jaoks eraldi unit testid.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Aine oop = new Aine("RT-016", "Objektorienteeritud programmeerimine", 5);
+        Oppekava kava = new Oppekava("RT");
+
+
+        System.out.println("Nimetus: " + oop.getNimetus());
+        System.out.println("Kood: " + oop.getKood());
+        System.out.println("EAP: " + oop.getEap());
+        System.out.println("Tundide arv: " + oop.tundideArv());
+        System.out.println(oop.toString() + "\n");
+
+
+        System.out.println(kava.toString());
+        kava.addAine(oop);
+        System.out.println("Kood: " + kava.getOppekavaKood());
+        System.out.println("Ained: " + kava.getAined());
+
+        System.out.println(kava.toString());
 
     }
+
 }
